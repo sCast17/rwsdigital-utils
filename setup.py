213f7e0d349cc24214094b03599b5ecd1be57bd7
Salvatore Castaldo <31545710+sCast17@users.noreply.github.com>
2022-06-07 16:57:02 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="rwsdigital-utils",
-    version="2022.6.7.7",
+    version="2022.6.7.8",
     description="Odoo Development Utils by RWS Digital",
     long_description=long_description,
     long_description_content_type="text/markdown",
